d0fc7c8add6549386e8b84f89be7907b8e891aa7
Chris Kyrouac <ckyrouac@redhat.com>
2018-11-01 16:32:45 -0400

    Add pagination, sorting to GET /tags

--- api/tags.py
+++ api/tags.py
@@ -1,5 +1,8 @@
+import asyncio
+
 from db.models import Tag
 from insights_connexion import responses
+from insights_connexion.db.gino import db
 
 
 async def _get_one_tag(id):
@@ -7,27 +10,35 @@ async def _get_one_tag(id):
 
 
 async def _update_tag(id, body):
-    existing_tag = await _get_one_tag(id)
+    async with db.transaction():
+        existing_tag = await _get_one_tag(id)
 
-    if existing_tag is None:
-        return responses.not_found()
+        if existing_tag is None:
+            return responses.not_found()
 
-    await existing_tag.update(**body).apply()
-    updated_tag = await _get_one_tag(id)
-    return responses.update(updated_tag.dump())
+        await existing_tag.update(**body).apply()
+        updated_tag = await _get_one_tag(id)
+        return responses.update(updated_tag.dump())
 
 
-async def search():
-    tags = await Tag.query.gino.all()
+async def search(page, page_size, sort_by, sort_dir, request=None):
+    sort_func = getattr(db, sort_dir)
+    [tags, count] = await asyncio.gather(
+        Tag.query.limit(page_size).offset(
+            page * page_size).order_by(sort_func(sort_by)).gino.all(),
+        db.scalar(db.select([db.func.count(Tag.id)]))
+    )
+
     tags_dump = [tag.dump() for tag in tags]
-    return responses.search(0, tags_dump)
+    return responses.search(count, tags_dump)
 
 
 async def post(request=None):
-    body = await request.json()
-    tag_to_create = Tag(**body)
-    created_tag = await tag_to_create.create()
-    return responses.create(created_tag.dump())
+    async with db.transaction():
+        body = await request.json()
+        tag_to_create = Tag(**body)
+        created_tag = await tag_to_create.create()
+        return responses.create(created_tag.dump())
 
 
 async def get(id):
